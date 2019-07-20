e7994a1569e4a3efc9e0e5d0a114f035b9e8c9be
unknown <kaspazza@gmail.com>
2019-07-20 16:17:56 +0200

    renamed modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './modules/shared/services/guards/auth.guard';
-import { SettingsRoutes } from './modules/settings/settings-routing.module';
-import { MusicRoutes } from './modules/music/music-routing.module';
+import { SettingsRoutes } from './modules/profile/profile-routing.module';
+import { MusicRoutes } from './modules/library/library-routing.module';
 import { UnsavedChangesGuard } from './modules/shared/services/guards/unsaved-changes.guard';
 
 const routes: Routes = [
