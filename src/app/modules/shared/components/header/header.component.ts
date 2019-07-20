015b812ccd7ae3af96aba743c2d8156c06d660f0
unknown <kaspazza@gmail.com>
2019-07-20 19:09:45 +0200

    populate shared module and create basic files

---
+++ src/app/modules/shared/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mto-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
+  })
+  export class HeaderComponent {
+    title = 'angular';
+  }
+  
