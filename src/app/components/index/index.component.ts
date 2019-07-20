825147ffe94252f077ac5198d68012ae503e4a93
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-07-20 21:14:38 +0200

    Generate slides in ngfor loop

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -17,6 +17,21 @@ export class IndexComponent implements OnInit {
   headElements = ["ID", "First", "Last", "Handle"];
 
   images = [1, 2, 3].map(() => "../../../assets/img/back.jpg");
+  imageDescriptions = [
+    {
+      label: "First slide label",
+      description: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+      label: "Second slide label",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      label: "Third slide label",
+      description:
+        "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+    }
+  ];
 
   ngOnInit() {}
 }
