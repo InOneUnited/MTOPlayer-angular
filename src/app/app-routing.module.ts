abdca4b4acff896a809e086907800802560bcd93
unknown <kaspazza@gmail.com>
2019-08-21 00:19:22 +0200

    commented not implemented parts of code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
 import { LibraryRoutes } from './modules/library/library-routing.module';
 import { ProfileRoutes } from './modules/profile/profile-routing.module';
 import { AuthGuard } from './modules/shared/services/guards/auth.guard';
@@ -8,21 +9,21 @@ import { UnsavedChangesGuard } from './modules/shared/services/guards/unsaved-ch
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'index',
-    pathMatch: 'full',
-    canActivate: [AuthGuard]
+    component: IndexComponent,
+    pathMatch: 'full'
+    // canActivate: [AuthGuard]
   },
   {
     path: 'profile',
-    children: ProfileRoutes,
-    canActivate: [AuthGuard],
-    canDeactivate: [UnsavedChangesGuard]
+    children: ProfileRoutes
+    // canActivate: [AuthGuard],
+    // canDeactivate: [UnsavedChangesGuard]
   },
   {
     path: 'library',
-    children: LibraryRoutes,
-    canActivate: [AuthGuard],
-    canDeactivate: [UnsavedChangesGuard]
+    children: LibraryRoutes
+    // canActivate: [AuthGuard],
+    // canDeactivate: [UnsavedChangesGuard]
   },
   {
     path: '**',
