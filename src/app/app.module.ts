bb7f9fde3e541ad4d1f433b2d78fa0d6032b10ca
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-08-21 14:55:37 +0200

    Initialize index-footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './components/index/index.component';
 import { SharedModule } from './modules/shared/shared.module';
+import { IndexFooterComponent } from './components/index/index-footer/index-footer/index-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, IndexComponent],
+  declarations: [AppComponent, IndexComponent, IndexFooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/components/index/index-footer/index-footer/index-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mto-index-footer',
+  templateUrl: './index-footer.component.html',
+  styleUrls: ['./index-footer.component.scss']
+})
+export class IndexFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
