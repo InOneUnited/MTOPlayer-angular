1d039ce1a048a66890faf2944470adc526d35248
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2019-08-21 15:49:42 +0200

    Correct grid styling

--- src/app/components/index/index-footer/index-footer/index-footer.component.ts
+++ src/app/components/index/index-footer/index-footer/index-footer.component.ts
@@ -9,10 +9,7 @@ export class IndexFooterComponent {
   constructor() {}
 
   elements: any = [
-    { id: 1, first: "Mark", last: "Otto", handle: "@mdo" },
-    { id: 2, first: "Jacob", last: "Thornton", handle: "@fat" },
-    { id: 3, first: "Larry", last: "the Bird", handle: "@twitter" }
+    { first: "Mark", last: "Otto", handle: "@mdo" },
+    { first: "Jacob", last: "Thornton", handle: "@fat" }
   ];
-
-  headElements = ["ID", "First", "Last", "Handle"];
 }
