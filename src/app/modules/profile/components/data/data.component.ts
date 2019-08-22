dd8d6209fd9493bfe036a0218f901d5012c0d84d
unknown <kaspazza@gmail.com>
2019-08-22 16:07:34 +0200

    created form

--- src/app/modules/profile/components/data/data.component.ts
+++ src/app/modules/profile/components/data/data.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 @Component({
   selector: 'mto-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.scss']
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
+  joinDateValue = new Date();
   form: FormGroup;
   userPicture: string;
-
-  ngOnInit() {
-    this.form = new FormGroup({});
+  email = 'ddd@o2.pl';
+  constructor(private fb: FormBuilder) {
+    this.form = this.createFormGroup();
   }
-
   onFileSelected(event) {
     console.log(event);
+  }
+
+  private createFormGroup() {
+    return this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      gender: [''],
+      birthday: [undefined]
+    });
   }
 
   onSave() {}
