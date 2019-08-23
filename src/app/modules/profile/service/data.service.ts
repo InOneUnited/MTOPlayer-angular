2dfd569300edff8b16ccb2bed7521bc421392d09
unknown <kaspazza@gmail.com>
2019-08-23 12:43:09 +0200

    added user to service

--- src/app/modules/profile/service/data.service.ts
+++ src/app/modules/profile/service/data.service.ts
@@ -1,7 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../../shared/model/user';
 
 @Injectable()
 export class DataService {
-  constructor(private http: HttpClient);
+  constructor(private http: HttpClient) {}
+
+  fetchUserData(userId: number) {
+    return new User();
+  }
 }
