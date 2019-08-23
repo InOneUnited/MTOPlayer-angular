fd846835b8a63e30f14bcba3fb0ab7a6ee320d3c
Mateusz Mazurczak <kaspazza@gmail.com>
2019-08-23 15:44:20 +0200

    Data/feature/profile (#26) (#27)
    * created fetch in service and updated model
    
    * implemented selector, state, reducers and actions

--- src/app/modules/profile/service/data.service.ts
+++ src/app/modules/profile/service/data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { User } from '../../shared/model/user';
 
 @Injectable()
@@ -7,6 +8,17 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   fetchUserData(userId: number) {
-    return new User();
+    return of(
+      new User({
+        id: userId,
+        email: 'jan_kowalski111@MatLine.pl',
+        firstName: 'Jan',
+        lastName: 'Kowalski',
+        gender: 'chłop',
+        userPicture: 'profile_picture.jpg',
+        birthday: new Date(1980, 2, 25),
+        joinDate: new Date(2018, 5, 15)
+      })
+    );
   }
 }
