20a83886c8172fc9d05c652920d5de6cf066399f
dardomanski <dardomanski@gmail.com>
2019-08-23 14:41:49 +0200

    Add Footer component to exports

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,39 +1,31 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { RouterModule } from "@angular/router";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        NgbModule,
-        ReactiveFormsModule,
-        RouterModule
-    ],
-  
-    declarations: [
-      HeaderComponent,
-      FooterComponent
-    ],
-  
-    entryComponents: [
-    ],
-  
-    exports: [
-      HeaderComponent
-    ]
-  })
-  export class SharedModule {
-    static forRoot(): ModuleWithProviders {
-      return {
-        ngModule: SharedModule,
-        providers: [
-        ],
-      };
-    }
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+
+  declarations: [HeaderComponent, FooterComponent],
+
+  entryComponents: [],
+
+  exports: [HeaderComponent, FooterComponent]
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: []
+    };
   }
-  
+}
