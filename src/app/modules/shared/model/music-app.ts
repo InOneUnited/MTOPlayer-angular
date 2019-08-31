aa780b10bbe6ec833e7e50dbae8ecd8b9b955b6e
unknown <kaspazza@gmail.com>
2019-08-31 12:18:27 +0200

    updated model

--- src/app/modules/shared/model/music-app.ts
+++ src/app/modules/shared/model/music-app.ts
@@ -1,11 +1,41 @@
+export enum ApiLogo {
+  dropbox = 'src/assets/img/logo/dropbox.jpg',
+  googleDrive = 'src/assets/img/logo/googledrive.png',
+  soundCloud = 'src/assets/img/logo/soundcloud.png',
+  spotify = 'src/assets/img/logo/spotify.png'
+}
+
 export class MusicApp {
   id: number = undefined;
   name: string = undefined;
+  userLogin: string = undefined;
   isConnected: boolean = undefined;
 
-  constructor(fields?: { id?: number; name?: string; isConnected?: boolean }) {
+  constructor(fields?: {
+    id?: number;
+    name?: string;
+    userLogin?: string;
+    isConnected?: boolean;
+  }) {
     if (fields) {
       Object.assign(this, fields);
+    }
+  }
+
+  get userLogo(): ApiLogo {
+    switch (this.name) {
+      case 'dropbox': {
+        return ApiLogo.dropbox;
+      }
+      case 'google_drive': {
+        return ApiLogo.googleDrive;
+      }
+      case 'spotify': {
+        return ApiLogo.spotify;
+      }
+      case 'soundcloud': {
+        return ApiLogo.soundCloud;
+      }
     }
   }
 }
