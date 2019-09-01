1acf4bd4df88551dc411fe957ced8e649caffc02
unknown <kaspazza@gmail.com>
2019-09-01 21:06:45 +0200

    created actions for add/delete app

--- src/app/modules/profile/store/actions/music-apps.actions.ts
+++ src/app/modules/profile/store/actions/music-apps.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+
 import { MusicApp } from '../../../shared/model/music-app';
 
 export const fetchMusicApps = createAction('[music apps] Fetch music apps');
@@ -10,5 +11,35 @@ export const fetchMusicAppsSuccess = createAction(
 
 export const fetchMusicAppsFailure = createAction(
   '[music apps] Fetch music apps failure',
+  props<{ failure: any }>()
+);
+
+export const addMusicApp = createAction(
+  '[music apps] Add new music app',
+  props<{ musicApp: MusicApp }>()
+);
+
+export const addMusicAppSuccess = createAction(
+  '[music apps] Add new music app success',
+  props<{ musicApp: MusicApp }>()
+);
+
+export const addMusicAppFailure = createAction(
+  '[music apps] Add new music app failure',
+  props<{ failure: any }>()
+);
+
+export const deleteMusicApp = createAction(
+  '[music apps] Delete music app',
+  props<{ musicApp: MusicApp }>()
+);
+
+export const deleteMusicAppSuccess = createAction(
+  '[music apps] Delete music app success',
+  props<{ musicApp: MusicApp }>()
+);
+
+export const deleteMusicAppFailure = createAction(
+  '[music apps] Delete music app failure',
   props<{ failure: any }>()
 );
