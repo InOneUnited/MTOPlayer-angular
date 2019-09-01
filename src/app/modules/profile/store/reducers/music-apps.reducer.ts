99a50fd39999e1381bcb963babcef4c669d13d3b
unknown <kaspazza@gmail.com>
2019-09-01 21:07:14 +0200

    created reducers for add/delete app

--- src/app/modules/profile/store/reducers/music-apps.reducer.ts
+++ src/app/modules/profile/store/reducers/music-apps.reducer.ts
@@ -1,21 +1,45 @@
 import { Action, createReducer, on } from '@ngrx/store';
+
 import * as MusicAppsActions from '../actions/music-apps.actions';
-import {
-  initialMusicAppsState,
-  MusicAppsState
-} from '../states/music-apps.state';
+import { initialMusicAppsState, MusicAppsState } from '../states/music-apps.state';
 
 const reducer = createReducer(
   initialMusicAppsState,
+  on(
+    MusicAppsActions.fetchMusicApps,
+    MusicAppsActions.addMusicApp,
+    MusicAppsActions.deleteMusicApp,
+    state => ({
+      ...state,
+      operationState: 'busy'
+    })
+  ),
+
   on(MusicAppsActions.fetchMusicAppsSuccess, (state, { musicApps }) => ({
     ...state,
-    currentMusicApps: musicApps
+    currentMusicApps: musicApps,
+    operationState: 'success'
   })),
 
-  on(MusicAppsActions.fetchMusicAppsFailure, state => ({
-    ...state,
-    operationState: 'error'
-  }))
+  on(
+    MusicAppsActions.addMusicAppSuccess,
+    MusicAppsActions.deleteMusicApp,
+    (state, { musicApp }) => ({
+      ...state,
+      currentMusicApp: musicApp,
+      operationState: 'success'
+    })
+  ),
+
+  on(
+    MusicAppsActions.fetchMusicAppsFailure,
+    MusicAppsActions.addMusicAppFailure,
+    MusicAppsActions.deleteMusicAppSuccess,
+    state => ({
+      ...state,
+      operationState: 'error'
+    })
+  )
 );
 
 export function musicAppsReducer(
