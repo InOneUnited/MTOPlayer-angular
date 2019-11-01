875afdf0ee8aa18e947fb1bb8bfcbdc562c6c485
Mateusz Mazurczak <kaspazza@gmail.com>
2019-11-01 12:45:57 +0100

    Data/feature/profile (#26) (#27) (#40)
    * created fetch in service and updated model
    
    * implemented selector, state, reducers and actions

--- src/app/modules/profile/store/effects/user.effects.ts
+++ src/app/modules/profile/store/effects/user.effects.ts
@@ -2,12 +2,20 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
+
 import { UserService } from '../../service/user.service';
-import { fetchUser, fetchUserSuccess, fetchUserFailure, updateUser, updateUserSuccess, updateUserFailure } from '../actions/user.actions';
+import {
+  fetchUser,
+  fetchUserFailure,
+  fetchUserSuccess,
+  updateUser,
+  updateUserFailure,
+  updateUserSuccess,
+} from '../actions/user.actions';
 
 @Injectable()
 export class UserEffects {
-  constructor(private actions$: Actions, private userService: UserService) {}
+  constructor(private actions$: Actions, private userService: UserService) { }
 
   fetchUser$ = createEffect(() =>
     this.actions$.pipe(
